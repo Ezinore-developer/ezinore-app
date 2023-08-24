1436f83c44ac8847ae62ba695ff3257dbebb7870
GM-11 <gmathur1001@gmail.com>
2023-08-24 16:49:00 +0530

    minor change

--- lib/loginflow/phonenoscreen.dart
+++ lib/loginflow/phonenoscreen.dart
@@ -6,7 +6,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 // import 'package:intl/intl.dart';
 
-// ignore: must_be_immutable
 class PhoneNoScreen extends ConsumerStatefulWidget {
   final VoidCallback onClick;
   final VoidCallback onClickBack;
